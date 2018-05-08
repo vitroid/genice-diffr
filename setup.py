8e32258c96253f4af1f481975bb8f82721e6b32e
Masakazu Matsumoto <vitroid@gmail.com>
2018-05-08 11:57:30 +0900

    Changed the document style to .md.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 # from distutils.core import setup, Extension
 from setuptools import setup, Extension
-from numpy.distutils.misc_util import get_numpy_include_dirs
 import os
 import codecs
 import re
@@ -13,13 +12,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(os.path.dirname(__file__), 'genice_diffr', '__init__.py'),
                  encoding='utf8') as version_file:
     metadata = dict(re.findall(r"""__([a-z]+)__ = "([^"]+)""", version_file.read()))
-    
+
+
+long_desc = "".join(open("README.md").readlines())
+
 
 setup(
     name='genice_diffr',
     version=metadata['version'],
     description='Diffraction plugin for GenIce.',
-    #long_description=README + '\n\n' +  CHANGES,
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
